14188561bdd2ff3bec9ebf5ae69198c4037b56c8
Chris Xu <gzchrisxjh@gmail.com>
2020-06-05 11:23:48 -0400

    Update binary search

---
+++ binarysearch/src/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,47 @@
+//Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+//
+//        Your algorithm's runtime complexity must be in the order of O(log n).
+//
+//        If the target is not found in the array, return [-1, -1].
+//
+//        Example 1:
+//
+//        Input: nums = [5,7,7,8,8,10], target = 8
+//        Output: [3,4]
+//        Example 2:
+//
+//        Input: nums = [5,7,7,8,8,10], target = 6
+//        Output: [-1,-1]
+
+import java.util.Arrays;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+    private static int binarySearch(int[] nums, int target, boolean left) {
+        int lo = 0;
+        int hi = nums.length;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] > target || (left && nums[mid] == target)) hi = mid;
+            else lo = mid + 1;
+        }
+
+        return lo;
+    }
+
+    private static int[] searchRange(int[] nums, int target) {
+        int left = binarySearch(nums, target, true);
+
+        if (left >= nums.length || nums[left] != target) return new int[]{-1, -1};
+
+        int right = binarySearch(nums, target, false) - 1;
+
+        return new int[] {left, right};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8,
+                8, 10}, 8)));
+    }
+}
---
+++ binarysearch/src/RandomPickWithWeight.java
@@ -0,0 +1,63 @@
+//Given an array w of positive integers, where w[i] describes the weight of index i,
+// write a function pickIndex which randomly picks an index in proportion to its weight.
+//
+//        Note:
+//
+//        1 <= w.length <= 10000
+//        1 <= w[i] <= 10^5
+//        pickIndex will be called at most 10000 times.
+//        Example 1:
+//
+//        Input:
+//        ["Solution","pickIndex"]
+//        [[[1]],[]]
+//        Output: [null,0]
+//        Example 2:
+//
+//        Input:
+//        ["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+//        [[[1,3]],[],[],[],[],[]]
+//        Output: [null,0,1,1,1,0]
+
+import java.util.Random;
+
+public class RandomPickWithWeight {
+
+    private class Solution {
+        private int totalWeight;
+
+        private int[] cdf;
+
+        Random random;
+
+        public Solution(int[] w) {
+            totalWeight = 0;
+            cdf = new int[w.length];
+            random = new Random();
+
+            for (int i = 0; i < w.length; ++i) {
+                totalWeight += w[i];
+                cdf[i] = totalWeight;
+            }
+        }
+
+        public int pickIndex() {
+            int target = random.nextInt(totalWeight);
+            int lo = 0;
+            int hi = cdf.length - 1;
+
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                if (cdf[mid] <= target) lo = mid + 1;
+                else hi = mid;
+            }
+
+            return lo;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
